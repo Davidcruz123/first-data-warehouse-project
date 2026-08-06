5edcd360e9d90694800ff91f0a865e2d59f00a8a
Davidcruz123 <69655046+Davidcruz123@users.noreply.github.com>
2026-08-05 21:45:23 -0600

    Add quality checks for silver layer data validation

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -17,3 +17,61 @@ Usage Notes:
     - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
+
+
+-- ====================================================================
+-- Checking 'silver.crm_cust_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+
+SELECT cst_id,COUNT(*)
+FROM silver.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*)>1 OR cst_id IS NULL
+ORDER BY cst_id
+
+-- Check for unwanted Spaces.
+-- Expectation: No results.
+SELECT *
+FROM silver.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname) OR cst_lastname != TRIM(cst_lastname)
+
+-- Data standarization and consistency
+
+SELECT DISTINCT cst_gndr
+FROM silver.crm_cust_info;
+
+SELECT DISTINCT cst_marital_status
+FROM silver.crm_cust_info;
+
+-- ====================================================================
+-- Checking 'silver.crm_prd_info'
+-- ====================================================================
+-- Check for NULLs or Duplicates in Primary Key
+-- Expectation: No Results
+
+SELECT COUNT(*) FROM silver.crm_prd_info
+GROUP BY prd_id
+HAVING COUNT(*) >1 OR prd_id IS NULL;
+
+-- Check for unwanted spaces
+-- Expectation: No Results
+SELECT * 
+FROM silver.crm_prd_info
+WHERE prd_nm != TRIM(prd_nm)
+
+-- Check for NULLs or Negative Values in Cost
+-- Expectation: No Results
+SELECT * 
+FROM silver.crm_prd_info
+WHERE prd_cost IS NULL OR prd_cost<0
+
+--Data standarization & consistency
+SELECT DISTINCT prd_line
+FROM silver.crm_prd_info
+
+--Check for invalid date orders (Start Date > End Date)
+SELECT * 
+FROM silver.crm_prd_info
+WHERE prd_start_dt> prd_end_dt
