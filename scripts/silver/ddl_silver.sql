cc4914363580573c20d2314f715e55399857ac24
Davidcruz123 <69655046+Davidcruz123@users.noreply.github.com>
2026-08-17 22:46:06 -0600

    Fix column name and update data types in SQL script

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -10,9 +10,16 @@ CREATE TABLE silver.crm_cust_info (
 	cst_key 		VARCHAR(50),
 	cst_firstname 	VARCHAR(50),
 	cst_lastname 	VARCHAR(50),
-	cst_marital_status VARCHAR(50),
+	cst_material_status VARCHAR(50),
 	cst_gndr 		VARCHAR(50),
-	cst_create_date TIMESTAMP,
+	cst_create
+    prd_id          INT,	
+    prd_key         VARCHAR(50),
+    prd_nm          VARCHAR(50),
+    prd_cost        INT,	
+    prd_line        VARCHAR(50),
+    prd_start_dt    TIMESTAMP,
+    prd_end_dt      TIMESTAMP,
 	dwh_create_date TIMESTAMP DEFAULT NOW()
 );
 
@@ -20,13 +27,12 @@ CREATE TABLE silver.crm_cust_info (
 DROP TABLE IF EXISTS silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
     prd_id          INT,	
-	cat_id			VARCHAR(50),
     prd_key         VARCHAR(50),
     prd_nm          VARCHAR(50),
     prd_cost        INT,	
     prd_line        VARCHAR(50),
-    prd_start_dt    DATE,
-    prd_end_dt      DATE,
+    prd_start_dt    TIMESTAMP,
+    prd_end_dt      TIMESTAMP,
 	dwh_create_date TIMESTAMP DEFAULT NOW()
 );
 
@@ -35,9 +41,9 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num     VARCHAR(50),
     sls_prd_key     VARCHAR(50),
     sls_cust_id     INT,
-    sls_order_dt    INT,
-    sls_ship_dt     INT,
-    sls_due_dt      INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
     sls_sales       INT,
     sls_quantity    INT,
     sls_price       INT,
