b2f00ca56704c87733c45da0b0161eb45dc3fc59
Davidcruz123 <69655046+Davidcruz123@users.noreply.github.com>
2026-08-20 14:48:02 -0600

    Add quality checks for silver and bronze tables
    Added quality checks for unwanted spaces and data consistency in 'bronze.erp_px_cat_g1v2'.

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -127,6 +127,28 @@ SELECT COUNT(*)
 FROM silver.erp_loc_a101
 WHERE cid LIKE '%-%';
 
+-- ====================================================================
+-- Checking 'silver.erp_px_cat_g1v2'
+-- ====================================================================
+
+-- Check for unwanted spaces
+-- Expectation: No Results
+SELECT id,cat,subcat,maintenance
+FROM bronze.erp_px_cat_g1v2
+WHERE cat!= TRIM(cat)
+ OR subcat != TRIM(subcat)
+ OR maintenance != TRIM(maintenance)
+
+--Data standarization and consistency
+SELECT DISTINCT cat
+FROM bronze.erp_px_cat_g1v2
+
+SELECT DISTINCT subcat
+FROM bronze.erp_px_cat_g1v2
+
+SELECT DISTINCT maintenance
+FROM bronze.erp_px_cat_g1v2
+
 -- Data standarization and consistency
 SELECT DISTINCT cntry
 FROM silver.erp_loc_a101;
