2d3f97116f39fa1bfb3051e1a8f1b9aff18a5797
Davidcruz123 <69655046+Davidcruz123@users.noreply.github.com>
2026-08-20 21:00:44 -0600

    Fix case sensitivity in usage example for stored procedure

--- scripts/silver/load_silver.sql
+++ scripts/silver/load_silver.sql
@@ -14,7 +14,7 @@ Parameters:
 	  This stored procedure does not accept any parameters or return any values.
 
 Usage Example:
-    Call Silver.load_silver;
+    Call silver.load_silver;
 ===============================================================================
 */
 
